7294f76db0ad2dcd991d4ab0a85182b0a0c729f4
liheming <liheming2679@gmail.com>
2018-08-24 20:42:46 +0800

    finished stack.

--- src/Stack/CR_Stack.java
+++ src/Stack/CR_Stack.java
@@ -1,4 +1,39 @@
 package Stack;
 
-public class CR_Stack {
+import java.util.Scanner;
+
+public class CR_Stack extends RL_Stack{
+
+    public static void main(String[] args){
+
+        RL_Stack st = new RL_Stack();
+        DATA data = new DATA();
+
+        StackType stack = st.STinit();
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("入栈操作：\n");
+        System.out.print("输入姓名，年龄，开始入栈。\n");
+        do{
+            DATA stackdata = new DATA();
+            stackdata.name = input.next();
+            if(stackdata.name.equals("0")){
+                break;
+            }else{
+                stackdata.age = input.nextInt();
+                st.PushST(stack,stackdata);
+            }
+        }while (true);
+
+        System.out.print("按任意非零键进行出栈：\n");
+        String temp = " ";
+        temp = input.next();
+        while (!temp.equals("0")){
+            data = st.PopST(stack);
+            System.out.printf("出栈数据是：（%s,%d)\n",data.name,data.age);
+            temp = input.next();
+        }
+        System.out.print("finished");
+        st.STFree(stack);
+    }
 }
--- src/Stack/RL_Stack.java
+++ src/Stack/RL_Stack.java
@@ -3,41 +3,65 @@ package Stack;
 public class RL_Stack implements Stack_Interface {
     @Override
     public StackType STinit() {
+        StackType p;
+        if((p = new StackType()) != null){
+            p.top = 0;
+            return p;
+        }
         return null;
     }
 
     @Override
     public boolean STIsEmpty(StackType s) {
-        return false;
+        boolean t;
+        t = (s.top == 0);
+        return t;
     }
 
     @Override
     public boolean STIsFull(StackType s) {
-        return false;
+        boolean t;
+        t = (s.top == MAXLength);
+        return t;
     }
 
     @Override
     public void STClear(StackType s) {
-
+        s.top = 0;
     }
 
     @Override
     public void STFree(StackType s) {
-
+        if(s != null){
+            s = null;
+        }
     }
 
     @Override
     public int PushST(StackType s, DATA data) {
-        return 0;
+        if((s.top + 1) > MAXLength){
+            System.out.print("栈溢出！\n");
+            return 0;
+        }
+        s.data[++s.top] = data;
+        return 1;
     }
 
     @Override
     public DATA PopST(StackType s) {
-        return null;
+        if(s.top == 0){
+            System.out.print("栈为空！\n");
+            System.exit(0);
+        }
+        return (s.data[s.top--]);
     }
 
     @Override
     public DATA PeekST(StackType s) {
-        return null;
+        if(s.top == 0){
+            System.out.print("栈为空！\n");
+            System.exit(0);
+        }
+        return (s.data[s.top]);
     }
 }
